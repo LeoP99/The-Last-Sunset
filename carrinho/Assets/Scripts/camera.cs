c575a759fab34e6c9d3ed424f793197b81424627
Felphz <felipejoliveira2@gmail.com>
2019-06-13 17:18:47 -0300

    carro movimentando realista

---
+++ carrinho/Assets/Scripts/camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+
+
+
+{
+
+
+    public GameObject player;
+
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = offset + player.transform.position;
+    }
+}
